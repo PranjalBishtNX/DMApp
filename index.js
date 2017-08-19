1cfa3a070cec4a5dae8926d6b2c222a7f107103a
PRANJALSLAPTOP\Pranjal <pranjalnitrox@gmail.com>
2017-08-19 19:05:04 +0530

    Changes in port for heroku

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require('express')
 , app = express()
 , server = require('http').createServer(app)
 , io = require("socket.io").listen(server)
-//... more config and code
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
+//app.set('port', process.env.PORT || 3000);  
+//app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
 //... code to continue
 
 app.get('/', function(req, res){
@@ -17,6 +16,6 @@ io.on('connection', function(socket){
   });
 });
 
-server.listen(port, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:' + port);
 });
