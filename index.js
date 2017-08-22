8fb2631b39b428738ea53c5e209b6308e658b4a0
PRANJALSLAPTOP\Pranjal <pranjalnitrox@gmail.com>
2017-08-22 16:52:59 +0530

    Added duel page resources and link

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express')
 //app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
 //... code to continue
 app.use('/style', express.static(__dirname + '/style'));
-
+app.use('/duel-files', express.static(__dirname+'/duel/duel-files'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -15,6 +15,7 @@ app.get('/', function (req, res) {
 app.get('/login', function (req, res) {
     //login controllers go here
     //trust me it will make sense in a few days
+    //it already makes sense nigga ~nitrox
 });
 
 app.get('/lobby', function (req, res) {
@@ -23,13 +24,15 @@ app.get('/lobby', function (req, res) {
 });
 
 app.get('/decks', function (req, res) {
-    //lobby controllers may check session before the reponse header is sent
     res.sendFile(__dirname + '/decks/decks.html');
 });
 
 app.get('/stats', function (req, res) {
-    //lobby controllers may check session before the reponse header is sent
     res.sendFile(__dirname + '/stats/stats.html');
+});
+
+app.get('/duel', function (req, res) {
+    res.sendFile(__dirname + '/duel/duel.html');
 });
 
 io.on('connection', function (socket) {
